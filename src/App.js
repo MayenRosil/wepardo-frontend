d1ed676c16e16f929b9f8e88d95d882354da187b
Julio Mayén <mayenrosil@gmail.com>
2024-02-27 15:22:56 -0600

    update api url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 const myFunction = async ()=>{
-  fetch('https://wepardo.services:3002/api/users', {
+  fetch('https://wepardo.services/api/users', {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
